7a1685ef77ac2feeb9c8b30af13f1b4415260df5
park <ruden91@naver.com>
2017-10-07 19:02:50 +0900

    install rimraf and finish code spliting webpack

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ import Routes from './router';
 import '../style/materialize.css';
 import '../style/react-range.css';
 import '../style/style.css';
-
 const App = () => {
   const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,41 @@
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// dependencies code spliting
+const VENDOR_LIBS = [
+  'faker','lodash','react','react-dom','react-input-range',
+  'react-redux','react-router','redux','redux-form','redux-thunk'
+];
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+    vendor: VENDOR_LIBS
+  },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: '[name].[chunkhash].js'
   },
-  module {
+  module: {
   	rules: [
 	  	{
 				use: 'babel-loader',
 				test: /\.js$/,
 				exclude: /node_modules/
-	  	}
+	  	},
+      {
+        use: ['style-loader', 'css-loader'],
+        test: /\.css$/
+      }
   	]
-  }
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['vendor', 'manifest']
+    }),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ]
 };
